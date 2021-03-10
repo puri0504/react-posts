392817fb6b8b83a02d3d18fad0da169b04a59ec2
ksenia.u <ksenia.u@find-bride.com>
2021-03-10 22:47:39 +0300

    added antd

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
+import { Row, Col } from 'antd';
 import Menu from  './components/Menu';
 import Post from  './components/Post';
 import './styles.css';
 
 function App() {
   return (
-    <div className="App">
-      <Menu />
-      <Post/>
-    </div>
+    <Row className="App">
+      <Col span={8}>
+        <Menu />
+      </Col>
+      <Col span={16}>
+        <Post/>
+      </Col>
+    </Row>
   );
 }
 
